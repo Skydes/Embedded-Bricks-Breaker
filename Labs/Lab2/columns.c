9e60aa27be0f4cb92c7e86a79f4f6fe7f8d043d6
Skydes <paul-edouard.sarlin@epfl.ch>
2017-03-02 17:55:33 +0800

    Fixed indent

--- Labs/Lab2/columns.c
+++ Labs/Lab2/columns.c
@@ -4,9 +4,9 @@
  * File:        columns.c
  * Date:        2017-03-02
  * Author:      Paul-Edouard Sarlin (A0153124U)
- * Comment:	    safe_printf is used to ensure that only one thread prints at one time.
- * 				do_sthing_mutex can also be used to ensure that only one thread
- * 				runs do_something at one time.
+ * Comment:     safe_printf is used to ensure that only one thread prints at one time.
+ *              do_sthing_mutex can also be used to ensure that only one thread
+ *              runs do_something at one time.
  *
  ***************************************************************************************/
 
@@ -24,59 +24,59 @@ pthread_t tid1, tid2, tid3, tid4;
 pthread_mutex_t do_sthing_mutex, printf_mutex;
 
 #define safe_printf(fmt,args...) { pthread_mutex_lock(&printf_mutex); \
-		xil_printf(fmt, ##args); fflush(stdout); \
-		pthread_mutex_unlock(&printf_mutex); }
+        xil_printf(fmt, ##args); fflush(stdout); \
+        pthread_mutex_unlock(&printf_mutex); }
 
 // ---------------------------------------
 
 volatile int taskrunning;
 
 void do_something(int max, int ID) {
-	int i,j;
-	//pthread_mutex_lock(&do_sthing_mutex);
-	for(i=0; i< max; i++) {
-		if (taskrunning != ID) {
-			taskrunning = ID;
-			safe_printf ("\r\n Column changed to : %d\r\n", ID);
-		}
-		safe_printf ("Microblaze 0: Column %d, i %d\r\n", ID, i);
-		for(j=0; j< 0xffff; j++) {
-			;
-		}
-	}
-	//pthread_mutex_unlock(&do_sthing_mutex);
+    int i,j;
+    //pthread_mutex_lock(&do_sthing_mutex);
+    for(i=0; i< max; i++) {
+        if (taskrunning != ID) {
+            taskrunning = ID;
+            safe_printf ("\r\n Column changed to : %d\r\n", ID);
+        }
+        safe_printf ("Microblaze 0: Column %d, i %d\r\n", ID, i);
+        for(j=0; j< 0xffff; j++) {
+            ;
+        }
+    }
+    //pthread_mutex_unlock(&do_sthing_mutex);
 }
 
 
 void* thread_func_1 () {
-	while(1) {
-		do_something(0x0004, 1);
-		sleep(200);
-	}
+    while(1) {
+        do_something(0x0004, 1);
+        sleep(200);
+    }
 }
 
 
 void* thread_func_2 () {
-	while(1) {
-		do_something(0x000f, 2);
-		sleep(100);
-	}
+    while(1) {
+        do_something(0x000f, 2);
+        sleep(100);
+    }
 }
 
 
 void* thread_func_3 () {
-	while(1) {
-		do_something(0x000f, 3);
-		sleep(10);
-	}
+    while(1) {
+        do_something(0x000f, 3);
+        sleep(10);
+    }
 }
 
 
 void* thread_func_4 () {
-	while(1) {
-		do_something(0x000f, 4);
-		sleep(10);
-	}
+    while(1) {
+        do_something(0x000f, 4);
+        sleep(10);
+    }
 }
 
 void* main_prog(void *arg);
@@ -84,85 +84,85 @@ void* main_prog(void *arg);
 int main (void) {
 
 
-	print("-- Entering main() uB0--\r\n");
-	xilkernel_init();
-	xmk_add_static_thread(main_prog,0);
-	xilkernel_start();
-	//Start Xilkernel
-	xilkernel_main ();
+    print("-- Entering main() uB0--\r\n");
+    xilkernel_init();
+    xmk_add_static_thread(main_prog,0);
+    xilkernel_start();
+    //Start Xilkernel
+    xilkernel_main ();
 
-	//Control does not reach here
+    //Control does not reach here
 }
 
 void* main_prog(void *arg) {
 
-	int ret;
+    int ret;
 
-	print("-- Entering main_prog() uB0--\r\n");
+    print("-- Entering main_prog() uB0--\r\n");
 
-	pthread_attr_init (&attr);
+    pthread_attr_init (&attr);
 
-	// initialize the SW Mutex
-	pthread_mutex_init (&do_sthing_mutex, NULL);
-	pthread_mutex_init(&printf_mutex, NULL);
+    // initialize the SW Mutex
+    pthread_mutex_init (&do_sthing_mutex, NULL);
+    pthread_mutex_init(&printf_mutex, NULL);
 
-	print("--Initialized -- uB0 \r\n");
-	// Priority 1 for thread 1
-	sched_par.sched_priority = 1;
-	pthread_attr_setschedparam(&attr,&sched_par);
-	print("--Priority Set uB0 --\r\n");
-	//start thread 1
-	ret = pthread_create (&tid1, &attr, (void*)thread_func_1, NULL);
-	print("--returned --\r\n");
-	if (ret != 0) {
-		xil_printf ("-- ERROR (%d) launching thread_func_1...\r\n", ret);
-	}
-	else {
-		xil_printf ("Thread 1 launched with ID %d \r\n",tid1);
-	}
-
-
-	// Priority 2 for thread 2
-	sched_par.sched_priority = 2;
-	pthread_attr_setschedparam(&attr,&sched_par);
-
-	//start thread 2
-	ret = pthread_create (&tid2, &attr, (void*)thread_func_2, NULL);
-	if (ret != 0) {
-		xil_printf ("-- ERROR (%d) launching thread_func_2...\r\n", ret);
-	}
-	else {
-		xil_printf ("Thread 2 launched with ID %d \r\n",tid2);
-	}
+    print("--Initialized -- uB0 \r\n");
+    // Priority 1 for thread 1
+    sched_par.sched_priority = 1;
+    pthread_attr_setschedparam(&attr,&sched_par);
+    print("--Priority Set uB0 --\r\n");
+    //start thread 1
+    ret = pthread_create (&tid1, &attr, (void*)thread_func_1, NULL);
+    print("--returned --\r\n");
+    if (ret != 0) {
+        xil_printf ("-- ERROR (%d) launching thread_func_1...\r\n", ret);
+    }
+    else {
+        xil_printf ("Thread 1 launched with ID %d \r\n",tid1);
+    }
 
 
-	// Priority 3 for thread 3
-	sched_par.sched_priority = 3;
-	pthread_attr_setschedparam(&attr,&sched_par);
+    // Priority 2 for thread 2
+    sched_par.sched_priority = 2;
+    pthread_attr_setschedparam(&attr,&sched_par);
 
-	//start thread 3
-	ret = pthread_create (&tid3, &attr, (void*)thread_func_3, NULL);
-	if (ret != 0) {
-		xil_printf ("-- ERROR (%d) launching thread_func_3...\r\n", ret);
-	}
-	else {
-		xil_printf ("Thread 3 launched with ID %d \r\n",tid3);
-	}
+    //start thread 2
+    ret = pthread_create (&tid2, &attr, (void*)thread_func_2, NULL);
+    if (ret != 0) {
+        xil_printf ("-- ERROR (%d) launching thread_func_2...\r\n", ret);
+    }
+    else {
+        xil_printf ("Thread 2 launched with ID %d \r\n",tid2);
+    }
 
 
-	// Priority 4 for thread 4
-	sched_par.sched_priority = 4;
-	pthread_attr_setschedparam(&attr,&sched_par);
+    // Priority 3 for thread 3
+    sched_par.sched_priority = 3;
+    pthread_attr_setschedparam(&attr,&sched_par);
 
-	//start thread 4
-	ret = pthread_create (&tid4, &attr, (void*)thread_func_4, NULL);
-	if (ret != 0) {
-		xil_printf ("-- ERROR (%d) launching thread_func_4...\r\n", ret);
-	}
-	else {
-		xil_printf ("Thread 4 launched with ID %d \r\n",tid4);
-	}
+    //start thread 3
+    ret = pthread_create (&tid3, &attr, (void*)thread_func_3, NULL);
+    if (ret != 0) {
+        xil_printf ("-- ERROR (%d) launching thread_func_3...\r\n", ret);
+    }
+    else {
+        xil_printf ("Thread 3 launched with ID %d \r\n",tid3);
+    }
 
-	return 0;
+
+    // Priority 4 for thread 4
+    sched_par.sched_priority = 4;
+    pthread_attr_setschedparam(&attr,&sched_par);
+
+    //start thread 4
+    ret = pthread_create (&tid4, &attr, (void*)thread_func_4, NULL);
+    if (ret != 0) {
+        xil_printf ("-- ERROR (%d) launching thread_func_4...\r\n", ret);
+    }
+    else {
+        xil_printf ("Thread 4 launched with ID %d \r\n",tid4);
+    }
+
+    return 0;
 }
 
--- Labs/Lab2/display_bricks_columns.c
+++ Labs/Lab2/display_bricks_columns.c
@@ -4,7 +4,7 @@
  * File:        display_bricks_columns.c
  * Date:        2017-03-02
  * Author:      Paul-Edouard Sarlin (A0153124U)
- * Comment:	    Set ps7_ddr_0_GP0_AXI_BASENAME base address to 0x10400000 in linker.
+ * Comment:     Set ps7_ddr_0_GP0_AXI_BASENAME base address to 0x10400000 in linker.
  *
  ***************************************************************************************/
 
@@ -22,26 +22,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define NB_COLUMNS 			8
-#define NB_RED_COLUMNS		2
-#define COLUMN_HEIGHT		240
-#define RATE				100
+#define NB_COLUMNS          8
+#define NB_RED_COLUMNS      2
+#define COLUMN_HEIGHT       240
+#define RATE                100
 
-#define TFT_DEVICE_ID    	XPAR_TFT_0_DEVICE_ID
-#define DDR_HIGH_ADDR    	XPAR_PS7_DDR_0_S_AXI_HIGHADDR
+#define TFT_DEVICE_ID       XPAR_TFT_0_DEVICE_ID
+#define DDR_HIGH_ADDR       XPAR_PS7_DDR_0_S_AXI_HIGHADDR
 
-#define NB_FRAMES			2
-#define TFT_FRAME_ADDR1    	0x10000000
-#define TFT_FRAME_ADDR2    	0x10200000
+#define NB_FRAMES           2
+#define TFT_FRAME_ADDR1     0x10000000
+#define TFT_FRAME_ADDR2     0x10200000
 
-#define RED					0x00ff0000
-#define BLUE				0x000000ff
-#define WHITE				0x00ffffff
-#define BLACK				0x00000000
+#define RED                 0x00ff0000
+#define BLUE                0x000000ff
+#define WHITE               0x00ffffff
+#define BLACK               0x00000000
 
 #define safe_printf(fmt,args...) { pthread_mutex_lock(&printf_mutex); \
-								   xil_printf(fmt, ##args); fflush(stdout); \
-								   pthread_mutex_unlock(&printf_mutex); }
+                                   xil_printf(fmt, ##args); fflush(stdout); \
+                                   pthread_mutex_unlock(&printf_mutex); }
 
 void* thread_display();
 int drawSolidBox(XTft *Tft, int x1, int y1, int x2, int y2, unsigned int col);
@@ -69,173 +69,173 @@ XGpio gpPB; //PB device instance
 
 /* Display thread */
 void* thread_display() {
-	int Status;
-	XTft_Config *TftConfigPtr;
-	u32 TftDeviceId = TFT_DEVICE_ID;
+    int Status;
+    XTft_Config *TftConfigPtr;
+    u32 TftDeviceId = TFT_DEVICE_ID;
 
-	u8 i, j, colors[NB_COLUMNS], colors_last[NB_FRAMES][NB_COLUMNS];
+    u8 i, j, colors[NB_COLUMNS], colors_last[NB_FRAMES][NB_COLUMNS];
 
-	/* Get address of the XTft_Config structure for the given device id */
-	TftConfigPtr = XTft_LookupConfig(TftDeviceId);
-	if (TftConfigPtr == (XTft_Config *)NULL)
-		return XST_FAILURE;
+    /* Get address of the XTft_Config structure for the given device id */
+    TftConfigPtr = XTft_LookupConfig(TftDeviceId);
+    if (TftConfigPtr == (XTft_Config *)NULL)
+        return XST_FAILURE;
 
-	/* Initialize all the TftInstance members */
-	Status = XTft_CfgInitialize(&TftInstance, TftConfigPtr, TftConfigPtr->BaseAddress);
-	if (Status != XST_SUCCESS)
-		return XST_FAILURE;
+    /* Initialize all the TftInstance members */
+    Status = XTft_CfgInitialize(&TftInstance, TftConfigPtr, TftConfigPtr->BaseAddress);
+    if (Status != XST_SUCCESS)
+        return XST_FAILURE;
 
-	/* Set background color to white and clean each buffer */
-	frames_cnt = 0;
-	for(i = 0; i < NB_FRAMES; i++) {
-		TftInstance.TftConfig.VideoMemBaseAddr = frames_addr[frames_cnt];
-		XTft_SetColor(&TftInstance, 0, WHITE);
-		XTft_ClearScreen(&TftInstance);
-		frames_cnt = nextCnt();
-		for(j = 0; j < NB_COLUMNS; j++)
-			colors_last[i][j] = -1;
-	}
-	TftInstance.TftConfig.VideoMemBaseAddr = frames_addr[frames_cnt];
+    /* Set background color to white and clean each buffer */
+    frames_cnt = 0;
+    for(i = 0; i < NB_FRAMES; i++) {
+        TftInstance.TftConfig.VideoMemBaseAddr = frames_addr[frames_cnt];
+        XTft_SetColor(&TftInstance, 0, WHITE);
+        XTft_ClearScreen(&TftInstance);
+        frames_cnt = nextCnt();
+        for(j = 0; j < NB_COLUMNS; j++)
+            colors_last[i][j] = -1;
+    }
+    TftInstance.TftConfig.VideoMemBaseAddr = frames_addr[frames_cnt];
 
-	while(1) {
+    while(1) {
 
-		/* Locally copy the columns states */
-		pthread_mutex_lock(&red_mutex);
-		memcpy(colors, col_is_red, NB_COLUMNS*sizeof(u8));
-		pthread_mutex_unlock(&red_mutex);
+        /* Locally copy the columns states */
+        pthread_mutex_lock(&red_mutex);
+        memcpy(colors, col_is_red, NB_COLUMNS*sizeof(u8));
+        pthread_mutex_unlock(&red_mutex);
 
-		/* Draw columns */
-		for(i = 0; i < NB_COLUMNS; i++) {
-			if(colors[i] != colors_last[frames_cnt][i]) {
-				u32 color = colors[i] ? RED : BLUE; // change to red for some values of i
-				drawSolidBox(&TftInstance, i*(XTFT_DISPLAY_WIDTH/NB_COLUMNS) + 1, 0,
-										   (i+1)*(XTFT_DISPLAY_WIDTH/NB_COLUMNS)-1 - 1,
+        /* Draw columns */
+        for(i = 0; i < NB_COLUMNS; i++) {
+            if(colors[i] != colors_last[frames_cnt][i]) {
+                u32 color = colors[i] ? RED : BLUE; // change to red for some values of i
+                drawSolidBox(&TftInstance, i*(XTFT_DISPLAY_WIDTH/NB_COLUMNS) + 1, 0,
+                                           (i+1)*(XTFT_DISPLAY_WIDTH/NB_COLUMNS)-1 - 1,
                                            COLUMN_HEIGHT, color);
-				colors_last[frames_cnt][i] = colors[i];
-			}
-		}
+                colors_last[frames_cnt][i] = colors[i];
+            }
+        }
 
-		/* Wait previous frame to be displayed */
-		while (XTft_GetVsyncStatus(&TftInstance) != XTFT_IESR_VADDRLATCH_STATUS_MASK);
-		/* Force display to the current frame buffer */
-		XTft_SetFrameBaseAddr(&TftInstance, frames_addr[frames_cnt]);
-		/* Switch frame counter */
-		frames_cnt = nextCnt();
-		/* Set the new frame address for subsequent draws */
-		TftInstance.TftConfig.VideoMemBaseAddr = frames_addr[frames_cnt];
-	}
+        /* Wait previous frame to be displayed */
+        while (XTft_GetVsyncStatus(&TftInstance) != XTFT_IESR_VADDRLATCH_STATUS_MASK);
+        /* Force display to the current frame buffer */
+        XTft_SetFrameBaseAddr(&TftInstance, frames_addr[frames_cnt]);
+        /* Switch frame counter */
+        frames_cnt = nextCnt();
+        /* Set the new frame address for subsequent draws */
+        TftInstance.TftConfig.VideoMemBaseAddr = frames_addr[frames_cnt];
+    }
 }
 
 void* thread_column (void *arg) {
-	u8 idx = *((int *) arg); // local context copy
-	free(arg);
-	safe_printf("Thread %d launched.\r\n", idx);
-	srand(idx);
-	u16 delay;
-	while(1) {
-		sem_wait(&sem);
-		pthread_mutex_lock(&red_mutex);
-		col_is_red[idx] = 1;
-		pthread_mutex_unlock(&red_mutex);
-		delay = rand() * (double) RATE / RAND_MAX;
-		safe_printf("Thread %u waiting %u.\r\n", idx, delay);
-		sleep(delay);
-		pthread_mutex_lock(&red_mutex);
-		col_is_red[idx] = 0;
-		pthread_mutex_unlock(&red_mutex);
-		sem_post(&sem);
-	}
+    u8 idx = *((int *) arg); // local context copy
+    free(arg);
+    safe_printf("Thread %d launched.\r\n", idx);
+    srand(idx);
+    u16 delay;
+    while(1) {
+        sem_wait(&sem);
+        pthread_mutex_lock(&red_mutex);
+        col_is_red[idx] = 1;
+        pthread_mutex_unlock(&red_mutex);
+        delay = rand() * (double) RATE / RAND_MAX;
+        safe_printf("Thread %u waiting %u.\r\n", idx, delay);
+        sleep(delay);
+        pthread_mutex_lock(&red_mutex);
+        col_is_red[idx] = 0;
+        pthread_mutex_unlock(&red_mutex);
+        sem_post(&sem);
+    }
 }
 
 u8 lastCnt() {
-	return (NB_FRAMES + frames_cnt + 1) % NB_FRAMES;
+    return (NB_FRAMES + frames_cnt + 1) % NB_FRAMES;
 }
 
 u8 nextCnt() {
-	return (NB_FRAMES + frames_cnt  - 1) % NB_FRAMES;
+    return (NB_FRAMES + frames_cnt  - 1) % NB_FRAMES;
 }
 
 int drawSolidBox(XTft *Tft, int x1, int y1, int x2, int y2, unsigned int col) {
-  int xmin,xmax,ymin,ymax,i,j;
+    int xmin,xmax,ymin,ymax,i,j;
 
-  if (x1 >= 0 &&
-      x1 <= XTFT_DISPLAY_WIDTH-1 &&
-      x2 >= 0 &&
-      x2 <= XTFT_DISPLAY_WIDTH-1 &&
-      y1 >= 0 &&
-      y1 <= XTFT_DISPLAY_HEIGHT-1 &&
-      y2 >= 0 &&
-      y2 <= XTFT_DISPLAY_HEIGHT-1) {
-    if (x2 < x1) {
-      xmin = x2;
-      xmax = x1;
-    }
-    else {
-      xmin = x1;
-      xmax = x2;
-    }
-    if (y2 < y1) {
-      ymin = y2;
-      ymax = y1;
-    }
-    else {
-      ymin = y1;
-      ymax = y2;
-    }
+    if (x1 >= 0 &&
+        x1 <= XTFT_DISPLAY_WIDTH-1 &&
+        x2 >= 0 &&
+        x2 <= XTFT_DISPLAY_WIDTH-1 &&
+        y1 >= 0 &&
+        y1 <= XTFT_DISPLAY_HEIGHT-1 &&
+        y2 >= 0 &&
+        y2 <= XTFT_DISPLAY_HEIGHT-1) {
+        if (x2 < x1) {
+            xmin = x2;
+            xmax = x1;
+        }
+        else {
+            xmin = x1;
+            xmax = x2;
+        }
+        if (y2 < y1) {
+            ymin = y2;
+            ymax = y1;
+        }
+        else {
+            ymin = y1;
+            ymax = y2;
+        }
 
-    for (i=xmin; i<=xmax; i++) {
-      for (j=ymin; j<=ymax; j++) {
-        XTft_SetPixel(Tft, i, j, col);
-      }
+        for (i=xmin; i<=xmax; i++) {
+            for (j=ymin; j<=ymax; j++) {
+                XTft_SetPixel(Tft, i, j, col);
+            }
+        }
+        return 0;
     }
-    return 0;
-  }
-  return 1;
+    return 1;
 }
 
 int main (void) {
-	print("-- Starting.\r\n");
+    print("-- Starting.\r\n");
 
-	xilkernel_init();
-	xmk_add_static_thread(main_prog, 0);
-	xilkernel_start();
+    xilkernel_init();
+    xmk_add_static_thread(main_prog, 0);
+    xilkernel_start();
 
-	return 0;
+    return 0;
 }
 
 void* main_prog(void *arg) {
-	int ret;
-	pthread_mutex_init(&printf_mutex, NULL);
-	pthread_mutex_init(&red_mutex, NULL);
-	sem_init(&sem, 1, NB_RED_COLUMNS);
+    int ret;
+    pthread_mutex_init(&printf_mutex, NULL);
+    pthread_mutex_init(&red_mutex, NULL);
+    sem_init(&sem, 1, NB_RED_COLUMNS);
 
-	for(int i = 0; i < NB_COLUMNS; i++)
-		col_is_red[i] = 0;
+    for(int i = 0; i < NB_COLUMNS; i++)
+        col_is_red[i] = 0;
 
-	xil_printf("--> Configuring threads.\r\n");
-	pthread_attr_init(&attr);
+    xil_printf("--> Configuring threads.\r\n");
+    pthread_attr_init(&attr);
 
-	/* Display management thread, priority 1 */
-	sched_par.sched_priority = 2;
-	pthread_attr_setschedparam(&attr, &sched_par);
-	ret = pthread_create (&tid_disp, &attr, (void*)thread_display, NULL);
-	if (ret != 0)
-		xil_printf ("-- ERROR (%d) launching thread_func_1...\r\n", ret);
-	else
-		xil_printf ("Thread 1 launched with ID %d \r\n",tid_disp);
+    /* Display management thread, priority 1 */
+    sched_par.sched_priority = 2;
+    pthread_attr_setschedparam(&attr, &sched_par);
+    ret = pthread_create (&tid_disp, &attr, (void*)thread_display, NULL);
+    if (ret != 0)
+        xil_printf ("-- ERROR (%d) launching thread_func_1...\r\n", ret);
+    else
+        xil_printf ("Thread 1 launched with ID %d \r\n",tid_disp);
 
-	/* One thread for each column, priority 1 */
-	sched_par.sched_priority = 1;
-	pthread_attr_setschedparam(&attr, &sched_par);
-	for (int i = 0; i < NB_COLUMNS; i++) {
-		int *arg = malloc(sizeof(arg));
-		if (arg == NULL) {
-			xil_printf("Couldn't allocate memory for thread column.\r\n");
-			continue;
-		}
-		*arg = i; // pass column index as argument
-		pthread_create(&tid_columns[i], &attr, (void*)thread_column, arg);
-	}
+    /* One thread for each column, priority 1 */
+    sched_par.sched_priority = 1;
+    pthread_attr_setschedparam(&attr, &sched_par);
+    for (int i = 0; i < NB_COLUMNS; i++) {
+        int *arg = malloc(sizeof(arg));
+        if (arg == NULL) {
+            xil_printf("Couldn't allocate memory for thread column.\r\n");
+            continue;
+        }
+        *arg = i; // pass column index as argument
+        pthread_create(&tid_columns[i], &attr, (void*)thread_column, arg);
+    }
 
-	return 0;
+    return 0;
 }
